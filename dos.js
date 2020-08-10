77b80e7315e05e145562664926b66f3b26119aa2
Oscar Ismael Yemha <68036403+OscarYemha@users.noreply.github.com>
2020-08-10 20:02:11 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -1,11 +1,71 @@
-/*
-2)De una compra debes ingresar una cantidad indeterminada de productos,
- validando los siguientes datos:
-marca, precio, peso en kilogramos, tipo(sólido o líquido)
-a)informar el peso total de la compra.
-b)la marca del más caro de los líquidos
-c)la marca del más barato de los sólidos
-*/function mostrar()
-{
-  alert("dos");
+function mostrar(){
+  
+  let tipoProducto;
+  let cantidadBolsas;
+  let precioPorBolsa;
+  let pregunta;
+  let importeBruto;
+  let importeTotalBruto = 0;
+  let importeDescuento;
+  let importeTotalDescuento = 0;
+  let tipoMasBolsas;
+  let mayorBolsas;
+  let flagMasBolsas = 0;
+  let flagMasCaro = 0;
+  let masCaro;
+  let tipoMasCaro;
+
+
+  do{
+    do{
+      tipoProducto = prompt("Ingrese el tipo de producto(arena, cal, cemento):")
+      tipoProducto = tipoProducto.toLowerCase();
+    }while(tipoProducto!=="arena" && tipoProducto!=="cal" && tipoProducto!=="cemento");
+
+    do{
+      cantidadBolsas = parseInt(prompt("Ingrese la cantidad de bolsas"));
+    }while(isNaN(cantidadBolsas));
+
+    do{
+      precioPorBolsa = parseFloat(prompt("Ingrese el precio por bolsa(mayor a cero):"));
+    }while(precioPorBolsa<=0);
+
+
+    importeBruto = cantidadBolsas * precioPorBolsa;
+    importeTotalBruto += importeBruto;
+
+
+    if(cantidadBolsas>30){
+      importeDescuento = importeBruto * 0.75;
+    }else if(cantidadBolsas>10){
+      importeDescuento = importeBruto * 0.85;
+    }else{
+      importeDescuento = importeBruto;
+    }
+
+
+    if(flagMasBolsas === 0 || mayorBolsas<cantidadBolsas){
+      mayorBolsas = cantidadBolsas;
+      tipoMasBolsas = tipoProducto;
+      flagMasBolsas = 1;
+    }
+
+
+    if(flagMasCaro === 0 || masCaro<precioPorBolsa){
+      masCaro = precioPorBolsa;
+      tipoMasCaro = tipoProducto;
+      flagMasCaro = 1;
+    }
+
+    importeTotalDescuento += importeDescuento;
+
+    pregunta = prompt("¿Desea seguir ingresando productos? Responda Si o No:")
+    pregunta = pregunta.toLowerCase();
+  }while(pregunta === "si");
+
+
+document.write("El importe bruto total a pagar es: " + importeTotalBruto + "<br>");
+document.write("El importe total a pagar con descuento es: " + importeTotalDescuento + "<br>");
+document.write("El tipo con más cantidad de bolsas es: " + tipoMasBolsas + "<br>");
+document.write("El tipo más caro es: " + tipoMasCaro);
 }
